02f7056ba8a15a2fa0f222e3faa53d7f0fba09ef
Daniel Spósito Coelho <spositocoelho@gmail.com>
2022-02-04 11:59:33 -0300

    terminado tutorial

--- matrixBackground.js
+++ matrixBackground.js
@@ -1,4 +1,4 @@
-/*o efeito matrix é feito com colunas de caracteres que caem de cima para baixo, com fade-out nas extremidades, iniciando de ponto aleatórios da coluna e com somente algumas colunas aleatorias aparecendo por vez*/
+/*o efeito matrix é feito com colunas de caracteres que caem de cima para baixo, com fade-out ao longo da queda, com somente algumas colunas aleatorias aparecendo por vez*/
 
 
 //pegando canvas do html
@@ -22,10 +22,11 @@ const columns = canvas.width/fontSize;
 const drops = new Array(Math.floor(columns)).fill(1); // o valor é iniciado como 1 para todos os elementos
 
 
+
 function draw()
 {
-    //pintando fundo de preto com opacidade, para valorizar o fade-out
-    background.fillStyle = 'rgba(0,0,0,0.05)';
+    //pintando fundo de preto com opacidade a cada repetição, para que as letras sumam aos poucos no contraste com o fundo transparente, mas que será predominantemente preto, pois pintaremos recursivamente o preto transparente, antes mesmo da página refazer os gráficos, usando requestAnimationFrame
+    background.fillStyle = 'rgba(0,0,0,0.08)';
     background.fillRect(0,0,canvas.width,canvas.height);
 
     //definiindo cor e estilo da fonte
@@ -38,8 +39,14 @@ function draw()
         const char = matrixChars[Math.floor(Math.random()*matrixChars.length)]
 
         //escrevendo na tela
-        background.fillText(char,i*fontSize, drops[i]*fontSize);
+        background.fillText(char,i*fontSize, drops[i]*fontSize); //x = iteração*tamanho da letra, assim a cada i a letra nova aparece ao lado da outra, com uma letra de espaço. 
+        drops[i]++ //movendo as colunas no eixo y, incrementando a partir de 1
+        if(drops[i] * fontSize > canvas.height && Math.random() > 0.95){ // para as gotas voltarem só em algums momentos ramdomicos, adicionamos uma condição de que o código dentro do if só acontece quando um numero gerado randomicamente é igual a um numero fixo qualquer determinado por nós
+            drops[i] = 0;   // se as letras atingirem o final da tela, elas devem voltar ao y=0
+        }
     }
+    //chamada recursiva para animar quadro a quadro, redesenhando a cadeia de chars
+    window.requestAnimationFrame(draw);
 }
 
 draw();
